dd669d16cac9fb5365aba96545bd3ca09bc501af
Fred M <frederic@moi.com>
2013-11-30 11:56:12 +0100

    Déplacement de la gestion utilisateur dans le répertoire Admin

---
+++ admin/user_delete.php
@@ -0,0 +1,52 @@
+<?php 
+	require 'database.php';
+	$id = 0;
+	
+	if ( !empty($_GET['id'])) {
+		$id = $_REQUEST['id'];
+	}
+	
+	if ( !empty($_POST)) {
+		// keep track post values
+		$id = $_POST['id'];
+		
+		// delete data
+		$pdo = Database::connect();
+		$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		$sql = "DELETE FROM user WHERE id = ?";
+		$q = $pdo->prepare($sql);
+		$q->execute(array($id));
+		Database::disconnect();
+		header("Location: user.php");
+		
+	} 
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <link   href="bootstrap.min.css" rel="stylesheet">
+    <script src="bootstrap.min.js"></script>
+</head>
+
+<body>
+    <div class="container">
+    			<div class="span10 offset1">
+    				<div class="row">
+		    			<h3>Suppression Utilisateur</h3>
+		    		</div>
+		    		
+	    			<form class="form-horizontal" action="user_delete.php" method="post">
+	    			  <input type="hidden" name="id" value="<?php echo $id;?>"/>
+					  <p class="alert alert-error">Confirmation de la suppression ?</p>
+					  <div class="form-actions">
+						  <button type="submit" class="btn btn-danger">Yes</button>
+						  <a class="btn" href="user.php">No</a>
+						</div>
+					</form>
+				</div>
+				
+    </div> <!-- /container -->
+  </body>
+</html>
