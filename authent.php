f9c2764fc5ee1d4f4cc6d366f7189abc3cb3a8f5
Fred M <frederic@moi.com>
2013-12-01 18:01:30 +0100

    Navigation avec Session Ok Login/Logout

---
+++ authent.php
@@ -0,0 +1,26 @@
+<?php
+class Authent{
+
+    static function islogged(){
+        //global $cnx;
+        if(isset($_SESSION['authent']) && isset($_SESSION['authent']['email']) && isset($_SESSION['authent']['password'])){
+            include 'database.php';
+            $pdo = Database::connect();
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $sql = 'SELECT email,password FROM user WHERE email = :email AND password = :password';
+            $q = $pdo->prepare($sql);
+            $q->execute(array('email'=>$_SESSION['authent']['email'],'password'=>$_SESSION['authent']['password']));
+            $data = $q->fetch(PDO::FETCH_ASSOC);
+            $count = $q->rowCount($sql);
+            Database::disconnect();
+                if($count == 1){
+                    return true;
+                } else {
+                    return false;
+                }
+        } else {
+            return false;
+        }
+    }
+}
+?>
