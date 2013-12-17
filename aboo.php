3a275a6a9eada2d5c58b4fd10b467bf8fe9efdac
Frédéric MEYROU <frederic.meyrou-externe@capgemini.com>
2013-12-17 14:49:53 +0100

    Test Nav TOP

---
+++ aboo.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <title>Aboo</title>
+    <meta charset="utf-8">
+    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css" media="screen">
+    <link rel='stylesheet' id='google_fonts-css'  href='http://fonts.googleapis.com/css?family=PT+Sans|Lato:300,400|Lobster|Quicksand' type='text/css' media='all' />
+	<style>
+		.navbar-inverse .navbar-brand {color: #9ABC00; font-family: Lobster,cursive; font-size: 48px; }
+		body { padding-top: 70px; }
+	</style>
+</head>
+
+<body>
+    <script src="bootstrap/js/jquery-2.0.3.min.js"></script>
+    <script src="bootstrap/js/bootstrap.min.js"></script>
+    
+    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">   	
+	  <!-- Marque -->
+	  <div class="navbar-header">
+	    <a class="navbar-brand" href="">Aboo</a>
+	  </div>	  
+	  <!-- Liens -->
+	  <div class="collapse navbar-collapse" id="TOP">
+	    <ul class="nav navbar-nav">
+	      <li><a href=""></a></li>	    	
+          <li><a href="connexion.php">Connexion</a></li>     	      
+	       <!--<li class="dropdown">
+	        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Menu Dropdown <b class="caret"></b></a>
+	        <ul class="dropdown-menu">
+	          <li><a href="#">Action</a></li>
+	          <li class="divider"></li>
+	          <li><a href="#">Action</a></li>
+	        </ul>
+	      </li>-->      
+	    </ul>
+	    <button type="button" class="btn btn-default navbar-btn pull-right">Connexion</button>
+	  </div><!-- /.navbar-collapse -->	  
+	</nav>
+
+    <div class="container">
+
+        <div class="alert alert alert-success alert-dismissable fade in">
+            <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+            <strong>Bonjour!</strong> Bienvenue sur l'application Aboo!
+        </div>
+
+    </div> <!-- /container -->
+  </body>
+</html>
