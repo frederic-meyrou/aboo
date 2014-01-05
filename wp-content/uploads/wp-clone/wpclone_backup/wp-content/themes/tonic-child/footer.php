b49350d9c8f0a3b93e4f316d34ddabee2564035b
Fred M <frederic@moi.com>
2014-01-05 17:15:23 +0100

    WP MaJ aboo

---
+++ wp-content/uploads/wp-clone/wpclone_backup/wp-content/themes/tonic-child/footer.php
@@ -0,0 +1,40 @@
+<?php
+/**
+Theme Name: Tonic
+Description: Thème enfant pour Tonic
+Author: FME
+Template: tonic
+**/
+ 
+/**
+ * The template for displaying the footer.
+ *
+ * Contains footer content and the closing of the
+ * #main, .grid and #page div elements.
+ *
+ * @since 1.0.0
+ */
+$bavotasan_theme_options = bavotasan_theme_options();
+
+		/* Do not display sidebar on home page if 'home_posts' turned
+			off */
+		if ( $bavotasan_theme_options['home_posts'] )
+			get_sidebar();
+		?>
+	</div> <!-- #main.row -->
+</div> <!-- #page.grid -->
+
+<footer id="footer" role="contentinfo">
+	<div id="footer-content" class="grid <?php echo $bavotasan_theme_options['width']; ?>">
+		<div class="row">
+			<p class="copyright c12">
+				<span class="fl"><?php printf( __( 'Copyright &copy; %s %s. All Rights Reserved.', 'tonic' ), date( 'Y' ), ' <a href="' . home_url() . '">' . get_bloginfo( 'name' ) .'</a>' ); ?></span>
+				<span class="credit-link fr"><i class="icon-leaf"></i><?php echo '<a href="' . home_url() . '/wp-login.php">Login</a>'; ?></span>
+			</p><!-- .c12 -->
+		</div><!-- .row -->
+	</div><!-- #footer-content.grid -->
+</footer><!-- #footer -->
+
+<?php wp_footer(); ?>
+</body>
+</html>
