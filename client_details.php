4a751d56bb097f07fb8665700fc45351913e0a4d
Frédéric MEYROU <frederic.meyrou-externe@capgemini.com>
2014-01-06 15:07:59 +0100

    Ajout de favicon et head.php

--- client_details.php
+++ client_details.php
@@ -103,19 +103,7 @@
 
 <!DOCTYPE html>
 <html lang="fr">
-<head>
-    <title>Aboo</title>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css" media="screen">
-    <link href="bootstrap/css/aboo.css" rel="stylesheet">
-    <link rel='stylesheet' id='google_fonts-css'  href='http://fonts.googleapis.com/css?family=PT+Sans|Lato:300,400|Lobster|Quicksand' type='text/css' media='all' />
-    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-      <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
-    <![endif]-->
-</head>
+<?php require 'head.php'; ?>
 
 <body>
     
