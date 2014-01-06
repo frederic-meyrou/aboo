3092c7d439ad7106945b8063b4d11f477594e56b
Fred M <frederic@moi.com>
2014-01-06 20:18:53 +0100

    Ajout de nav.php et footer.php et impacts

--- abo.php
+++ abo.php
@@ -264,42 +264,8 @@
 <?php require 'head.php'; ?>
 
 <body>
-    
-    <script src="bootstrap/js/jquery-2.0.3.min.js"></script>
-    <script src="bootstrap/js/bootstrap.min.js"></script>
 
-    <!-- Affiche la navigation -->
-    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">      
-      <div class="navbar-header">
-          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <!-- Marque -->
-          <a class="navbar-brand" href="home.php">Aboo</a>
-      </div>     
-      <!-- Liens -->
-      <div class="collapse navbar-collapse" id="TOP">
-        <ul class="nav navbar-nav">
-          <li class="active"><a href="journal.php"><span class="glyphicon glyphicon-th-list"></span> Recettes & Dépenses</a></li>
-          <li><a href="bilan.php"><span class="glyphicon glyphicon-calendar"></span> Bilan</a></li>
-          <li><a href="paiements.php"><span class="glyphicon glyphicon-euro"></span> Paiements</a></li>
-          <li><a href="mesclients.php"><span class="glyphicon glyphicon-star"></span> Clients</a></li>                           
-          <li class="dropdown">
-	        <!-- Affiche le nom de l'utilisateur à droite de la barre de Menu -->
-            <a href="#" class="dropdown-toggle" data-toggle="dropdown"><span class="glyphicon glyphicon-user"></span> <?php echo ucfirst($prenom) . ' ' . ucfirst($nom); ?><b class="caret"></b></a>
-            <ul class="dropdown-menu">
-              <li><a href="conf.php"><span class="glyphicon glyphicon-wrench"></span> Configuration</a></li>
-              <li><a href="debug.php"><span class="glyphicon glyphicon-info-sign"></span> Debug</a></li>  
-              <li><a href="deconnexion.php"><span class="glyphicon glyphicon-off"></span> Deconnexion</a></li>  
-            </ul> 
-          </li>
-          <li><a href="deconnexion.php"><span class="glyphicon glyphicon-off"></span></a></li>      
-        </ul>
-      </div><!-- /.navbar-collapse -->
-    </nav>
+    <?php $page_courante = "journal.php"; require 'nav.php'; ?>
         
     <div class="container">
         <h2>Recettes & Abonnements</h2>
@@ -500,5 +466,8 @@
         </div>  <!-- /span -->        			
     
     </div> <!-- /container -->
+
+    <?php require 'footer.php'; ?>
+        
   </body>
 </html>
---
+++ nav.php
@@ -0,0 +1,42 @@
+<?php
+    if (!isset($page_courante)) {
+        $page_courante = null;
+    }
+    if (!isset($nom) || !isset($prenom)) {
+        $prenom = 'utilisateur';
+        $nom = 'inconnu';
+    }    
+?>
+
+<!-- Affiche la navigation -->
+<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">      
+  <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <!-- Marque -->
+      <a class="navbar-brand" href="home.php">Aboo</a>
+  </div>     
+  <!-- Liens -->
+  <div class="collapse navbar-collapse" id="TOP">
+    <ul class="nav navbar-nav">
+      <li<?php echo ($page_courante == "journal.php")?' class="active"':'';?>><a href="journal.php"><span class="glyphicon glyphicon-th-list"></span> Recettes & Dépenses</a></li>
+      <li<?php echo ($page_courante == "bilan.php")?' class="active"':'';?>><a href="bilan.php"><span class="glyphicon glyphicon-calendar"></span> Bilan</a></li>
+      <li<?php echo ($page_courante == "paiements.php")?' class="active"':'';?>><a href="paiements.php"><span class="glyphicon glyphicon-euro"></span> Paiements</a></li>
+      <li<?php echo ($page_courante == "mesclients.php")?' class="active"':'';?>><a href="mesclients.php"><span class="glyphicon glyphicon-star"></span> Clients</a></li>                           
+      <li class="dropdown">
+        <!-- Affiche le nom de l'utilisateur à droite de la barre de Menu -->
+        <a href="#" class="dropdown-toggle" data-toggle="dropdown"><span class="glyphicon glyphicon-user"></span> <?php echo ucfirst($prenom) . ' ' . ucfirst($nom); ?><b class="caret"></b></a>
+        <ul class="dropdown-menu">
+          <li<?php echo ($page_courante == "conf.php")?' class="active"':'';?>><a href="conf.php"><span class="glyphicon glyphicon-wrench"></span> Configuration</a></li>
+          <li<?php echo ($page_courante == "debug.php")?' class="active"':'';?>><a href="debug.php"><span class="glyphicon glyphicon-info-sign"></span> Debug</a></li>  
+          <li><a href="deconnexion.php"><span class="glyphicon glyphicon-off"></span> Deconnexion</a></li>
+        </ul> 
+      </li>
+      <li><a href="deconnexion.php"><span class="glyphicon glyphicon-off"></span></a></li>      
+    </ul>
+  </div><!-- /.navbar-collapse -->
+</nav>
