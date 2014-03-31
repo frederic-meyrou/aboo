99f7ac23dcbd06eb5121f058a0cd12246f91b36c
Frederic Meyrou <frederic.meyrou@aboo.fr>
2014-03-31 19:13:13 +0200

    Boutons en affichage xs dans tableaux

--- aboo/user.php
+++ aboo/user.php
@@ -184,12 +184,12 @@
 							echo ($row['administrateur']==1)?'Oui':'Non' . '</td>';                                
 						   	echo '<td width=90>';
 				  ?>	
-							<div class="btn-group btn-group-sm">
-								  	<a href="user_update.php?id=<?php echo $row['id']; ?>" class="btn btn-default btn-sm btn-warning glyphicon glyphicon-edit" role="button"> </a>
+							<div class="btn-group btn-group-xs">
+								  	<a href="user_update.php?id=<?php echo $row['id']; ?>" class="btn btn-default btn-xs btn-warning glyphicon glyphicon-edit" role="button"> </a>
 								  	<!-- Le bonton Delete active la modal et modifie le champ value à la volée pour passer l'ID a supprimer en POST -->
 								  	<a href="#" id="<?php echo $row['id']; ?>"
 								  	   onclick="$('#DeleteInput').val('<?php echo $row['id']; ?>'); $('#modalDelete').modal('show'); "
-								  	   class="btn btn-default btn-sm btn-danger glyphicon glyphicon-trash" role="button"> </a>								  	
+								  	   class="btn btn-default btn-xs btn-danger glyphicon glyphicon-trash" role="button"> </a>								  	
 							</div>
 							
 						   	</td>						
