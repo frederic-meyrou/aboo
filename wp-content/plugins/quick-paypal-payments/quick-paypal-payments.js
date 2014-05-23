a5030dc170b7132509fcc6496c1ce90780477869
Frédéric MEYROU <frederic.meyrou-externe@capgemini.com>
2014-05-23 11:37:02 +0200

    WP Modif extensions

--- wp-content/plugins/quick-paypal-payments/quick-paypal-payments.js
+++
@@ -1,3 +0,0 @@
-function qppclear(thisfield, defaulttext) {if (thisfield.value == defaulttext) {thisfield.value = "";}}
-function qpprecall(thisfield, defaulttext) {if (thisfield.value == "") {thisfield.value = defaulttext;}}
-function replaceContentInContainer(target,source) {document.getElementById(target).innerHTML  = document.getElementById(source).innerHTML; }
